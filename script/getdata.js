ee05edd3a6e09ef2a7d71cc55fa498c68a9da259
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-10-27 05:29:22 +0200

    creation of folders

--- script/getdata.js
+++ script/getdata.js
@@ -1,28 +1,3 @@
-/*const copyright = document.querySelector("#copyright")
-
-copyright.textContent = `© ${new Date().getFullYear()}`;
-
-let lastModified = document.lastModified;
-document.querySelector(".last-modified").innerHTML = lastModified;
-
-const pattern = /last_modif\s*=\s*([^;]*)/;
-
-if (
-  Date.parse(document.lastModified) >
-  (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
-) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
-    location.pathname
-  }`;
-  alert("This page has changed!");
-}
-
-const menuButton = document.querySelector('#menuButton');
-
-menuButton.addEventListener('click', function() {
-    toggleMenu();
-});*/
-
 let d = new Date();
 
 let newUpdate = document.getElementById('footer-sect');
