3ed206f72d06da015b3f914345bb58d719a1a985
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-11-25 20:24:09 +0200

    fixing directory

--- chamber of commerce/script/directory.js
+++ chamber of commerce/script/directory.js
@@ -1,2 +1,44 @@
+// chamber/scripts/chamber.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const membersContainer = document.getElementById("members-container");
+  const gridViewToggle = document.getElementById("grid-view");
+  const listViewToggle = document.getElementById("list-view");
+
+  gridViewToggle.addEventListener("change", toggleView);
+  listViewToggle.addEventListener("change", toggleView);
+
+  function toggleView() {
+    membersContainer.classList.toggle("grid-view", gridViewToggle.checked);
+    membersContainer.classList.toggle("list-view", listViewToggle.checked);
+  }
+
+  // Fetch and display members from JSON
+  fetch("data/members.json")
+    .then(response => response.json())
+    .then(data => displayMembers(data.members));
+});
+
+function displayMembers(members) {
+  const membersContainer = document.getElementById("members-container");
+  membersContainer.innerHTML = "";
+
+  members.forEach(member => {
+    const memberElement = document.createElement("div");
+    memberElement.classList.add(membersContainer.classList.contains("grid-view") ? "member-card" : "member-list-item");
+
+    memberElement.innerHTML = `
+      <h3>${member.name}</h3>
+      <p>Address: ${member.address}</p>
+      <p>Phone: ${member.phone}</p>
+      <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+      <img src="images/${member.image}" alt="${member.name} Logo">
+      <p>Membership Level: ${member.membershipLevel}</p>
+      <p>${member.otherInfo}</p>
+    `;
+
+    membersContainer.appendChild(memberElement);
+  });
+}
 
     
